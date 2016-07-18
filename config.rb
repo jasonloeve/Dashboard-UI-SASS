e769b967d96475a9f986e8d7cf02b0eed757ac0a
Jason <jason@wwc.co.za>
2016-07-18 18:56:55 +0200

    Load build to git

---
+++ config.rb
@@ -0,0 +1,14 @@
+######################
+# Compile sass from
+# workspace
+######################
+
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'stylesheets'
+sass_dir = 'sass'
+images_dir = 'images'
+javascripts_dir = 'jquery'
+relative_assets = true
+line_comments = false
+output_style = :compressed
